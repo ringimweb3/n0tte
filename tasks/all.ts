3388e2f88f1f29e90eee6af373fc110471463e93
ringimweb3 <ringimweb3@gmail.com>
2024-07-15 12:06:44 -0700

    Update all.ts
    Usage
    When initializing a new Fhenix Client we need to inject it with a provider - this can be a provider from any of your favorite web3 libraries - ethers, hardhat, web3js, etc.

--- tasks/all.ts
+++ tasks/all.ts
@@ -11,4 +11,21 @@ task("all").setAction(async function (
     await hre.run('set:weights');
     await hre.run('set:quantized-data');
     await hre.run('inference');
-});
+});https://github.com/ocskiurity/n0tte.gitimport { FhenixClient } from 'fhenixjs';
+import { JsonRpcProvider } from 'ethers';
+
+// initialize your web3 provider
+const provider = new JsonRpcProvider('https://api.helium.fhenix.zone');
+
+// initialize Fhenix Client
+const client = new FhenixClient({provider});
+
+// to encrypt data for a Fhenix contract
+let encrypted = await client.encrypt(5, EncryptionTypes.uint8);
+
+// ...
+// contract logic goes here
+// ...
+
+// to unseal data returned from a Fhenix contract
+const cleartext = client.unseal(contractAddress, sealed);
